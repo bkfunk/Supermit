934bc5d97af27d40e46568dfa83cc90584610151
Brian K <brian@hyphenatemag.com>
2012-03-16 12:51:20 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supermit::Application.config.secret_token = 'cdfe0f8dcf0c52d0a5c68b2962cc7af848027186fec37e9b56d845ad29020f7263016eb0388f614f82c9a4af8f279aecff9817d90eebd4f9dbf63f54a1e26870'
